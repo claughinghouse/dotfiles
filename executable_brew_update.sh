e1883a998a478397cdc6fb9768a37ed13e4c2e7a
claughinghouse <claughinghouse@users.noreply.github.com>
2023-04-14 19:45:08 -0400

    Update brew_update.sh

--- executable_brew_update.sh
+++ executable_brew_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-brew update && brew upgrade && brew cleanup && brew bundle && chezmoi re-add && gcloud components update -q
+brew update && brew upgrade && brew cleanup && brew bundle --force && chezmoi re-add && gcloud components update -q
