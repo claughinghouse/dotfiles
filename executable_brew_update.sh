20b6479212c8e6bbc4772e1d72315e53210f7259
claughinghouse <claughinghouse@users.noreply.github.com>
2023-06-19 13:38:14 -0400

    Update brew_update.sh

--- executable_brew_update.sh
+++ executable_brew_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-brew update && brew upgrade && brew cleanup && brew bundle dump --force && brew bundle cleanup --force && chezmoi re-add
+brew update && brew upgrade && brew upgrade --cask --greedy && brew cleanup && brew bundle dump --force && brew bundle cleanup --force && chezmoi re-add
