54e4ce3fc3c525948ff86f2d327b27e1a5142ba6
claughinghouse <claughinghouse@users.noreply.github.com>
2024-03-31 12:00:04 -0400

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/auto-dark-mode.lua Add .config/nvim/lua/plugins/colorscheme.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/treesitter.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,18 @@
+-- return {
+--   "f-person/auto-dark-mode.nvim",
+--   config = {
+--     update_interval = 10000,
+--     set_dark_mode = function()
+--       vim.api.nvim_set_option("background", "dark")
+--       -- vim.cmd("colorscheme flexoki-dark")
+--       vim.cmd("colorscheme catppuccin-mocha")
+--     end,
+--     set_light_mode = function()
+--       vim.api.nvim_set_option("background", "light")
+--       -- vim.cmd("colorscheme flexoki-light")
+--       vim.cmd("colorscheme catppuccin-latte")
+--     end,
+--   },
+-- }
+
+return {}
