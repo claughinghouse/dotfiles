0d18ea2d8bf60ab8cff9bc44234a1bb52ab8f387
claughinghouse <claughinghouse@users.noreply.github.com>
2024-07-22 14:12:39 -0400

    just checking if git is working properly...

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.coding.codeium" },
+    -- { import = "lazyvim.plugins.extras.coding.codeium" },
 
     { import = "lazyvim.plugins.extras.util.chezmoi" },
     -- import/override with your plugins
