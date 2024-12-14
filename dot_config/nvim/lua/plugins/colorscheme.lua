99196aa4fa251c4bb0a94f376a448da87a0ac3b8
claughinghouse <claughinghouse@users.noreply.github.com>
2024-12-14 16:58:44 +0000

    apparently i did something�

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,20 @@
--- return {
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "dracula_pro",
---     },
---   },
---   -- This is managed by chezmoi. Its stored encrypted in this repo and placed in ~/.local/share/nvim/lazy/dracula_pro when `chezmoi apply` is run
---   {
---     "dracula_pro",
---   },
+--  return {
+--    {
+--      "LazyVim/LazyVim",
+--      opts = {
+--        colorscheme = "torte",
+--      }
+--    }
 -- }
+
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "night",
-    })
-    vim.cmd([[colorscheme tokyonight]])
-  end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			style = "night",
+		})
+		vim.cmd([[colorscheme tokyonight]])
+	end,
 }
