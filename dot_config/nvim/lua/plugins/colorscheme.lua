bf9f5b23551e2e5be2054106536c19dff6fceb90
claughinghouse <claughinghouse@users.noreply.github.com>
2024-12-14 12:06:31 -0500

    It works on my computer

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -8,13 +8,25 @@
 -- }
 
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("tokyonight").setup({
-			style = "night",
-		})
-		vim.cmd([[colorscheme tokyonight]])
-	end,
+  -- "folke/tokyonight.nvim",
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function()
+  --   require("tokyonight").setup({
+  --     -- style = "night",
+  --   })
+  -- vim.cmd([[colorscheme tokyonight]])
+  -- end,
+
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.sonokai_enable_italic = true
+      vim.cmd.colorscheme("sonokai")
+    end,
+  },
 }
