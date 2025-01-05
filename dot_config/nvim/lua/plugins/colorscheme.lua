c4c17606f64c7da428f3544e8c1c40729d62de5e
claughinghouse <claughinghouse@users.noreply.github.com>
2025-01-05 03:26:28 +0000

    Fix the fixes

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,11 @@
---  return {
---    {
---      "LazyVim/LazyVim",
---      opts = {
---        colorscheme = "torte",
---      }
---    }
--- }
-
 return {
-  -- "folke/tokyonight.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- config = function()
-  --   require("tokyonight").setup({
-  --     -- style = "night",
-  --   })
-  -- vim.cmd([[colorscheme tokyonight]])
-  -- end,
-
-  { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
-  vim.cmd([[colorscheme catppuccin]]),
+ {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end
+  }
 }
