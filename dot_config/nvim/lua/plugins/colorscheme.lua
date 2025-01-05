10cc0dbfd32bbfbb0af24908b9b7b924a43ec130
claughinghouse <claughinghouse@users.noreply.github.com>
2025-01-04 21:57:11 -0500

    Just stop reading these for a while, ok..

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -18,34 +18,6 @@ return {
   -- vim.cmd([[colorscheme tokyonight]])
   -- end,
 
-  {
-    "sainnhe/sonokai",
-    lazy = false,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.sonokai_enable_italic = true
-      -- vim.cmd.colorscheme("sonokai")
-    end,
-  },
-  {
-    "navarasu/onedark.nvim",
-    -- opts = {
-    --   style = "cool",
-    -- },
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    config = function()
-      require("nightfox").setup({})
-      vim.cmd([[colorscheme nightfox]])
-    end,
-  },
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "nightfox",
-  --   },
-  -- },
+  { "catppuccin/nvim", name = "catppuccin" },
+  vim.cmd([[colorscheme catppuccin]]),
 }
