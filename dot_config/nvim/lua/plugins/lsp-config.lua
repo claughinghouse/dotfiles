2af0b132dc836a020c7cb889526a3f06bff8ea1f
claughinghouse <claughinghouse@users.noreply.github.com>
2025-05-13 13:57:56 +0000

    [Insert your commit message here. Be sure to make it descriptive.]

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -24,6 +24,14 @@ return {
     config = function()
       local capabilities = require("blink.cmp").get_lsp_capabilities()
       local lspconfig = require("lspconfig")
+
+      local get_intelephense_license = function()
+        local f = assert(io.open(os.getenv("HOME") .. "/.config/intelephense/license.txt", "rb"))
+        local content = f:read("*a")
+        f:close()
+        return string.gsub(content, "%s+", "")
+      end
+
       lspconfig.astro.setup({ capabilities = capabilities })
       lspconfig.biome.setup({ capabilities = capabilities })
       lspconfig.cssls.setup({ capabilities = capabilities })
@@ -41,6 +49,9 @@ return {
           },
         },
         capabilities = capabilities,
+        init_options = {
+          licenceKey = get_intelephense_license(),
+        },
       })
     end,
   },
