6482d38097fafe6c71dc450c3425d61094b841cc
claughinghouse <claughinghouse@users.noreply.github.com>
2025-05-17 01:30:23 +0000

    It works on my computer

--- dot_config/nvim/lua/plugins/laravel.lua
+++ dot_config/nvim/lua/plugins/laravel.lua
@@ -1,7 +1,26 @@
-return {}
+-- return {}
 -- return {
 --   {
 --     "ricardoramirezr/blade-nav.nvim",
 --     ft = { "blade", "blade.php" },
 --   },
 -- }
+
+return {
+  "adalessa/laravel.nvim",
+  dependencies = {
+    "tpope/vim-dotenv",
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "kevinhwang91/promise-async",
+  },
+  cmd = { "Laravel" },
+  keys = {
+    { "<leader>la", ":Laravel artisan<cr>" },
+    { "<leader>lr", ":Laravel routes<cr>" },
+    { "<leader>lm", ":Laravel related<cr>" },
+  },
+  event = { "VeryLazy" },
+  opts = {},
+  config = true,
+}
