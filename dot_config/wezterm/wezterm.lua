3bae87dfba3b6779a6fb299e878a0fafeeccb0e2
claughinghouse <claughinghouse@users.noreply.github.com>
2026-05-05 16:39:51 -0400

    that coulda been bad

--- dot_config/wezterm/wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require("wezterm")
-local config = {}
-
-config.color_scheme = "Dracula+"
-config.font_size = 14
-config.font = wezterm.font("TX-02")
-config.window_close_confirmation = "NeverPrompt"
-
-return config
