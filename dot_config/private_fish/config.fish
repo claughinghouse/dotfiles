6bbed8329c313d781aac0a263608b3dbe15241ba
claughinghouse <claughinghouse@users.noreply.github.com>
2026-06-27 14:15:29 +0000

    hmmm

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,6 +7,12 @@ set -gx EDITOR nvim
 # Set neovim as the program to open manpages
 set -gx MANPAGER 'nvim +Man!'
 
+# Configure Plannotator when connected to the dev server over SSH
+if set -q SSH_CONNECTION; and test (hostname -s) = dev
+    set -gx PLANNOTATOR_REMOTE 1
+    set -gx PLANNOTATOR_PORT 9999
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
